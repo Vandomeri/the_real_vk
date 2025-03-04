b1dacdccf0468b6d22ee6379aead4a1fa5ba179e
Vando <vandomeri@gmail.com>
2025-03-04 12:59:45 +0300

    fixed profile form

--- app/profile/ProfileForm.jsx
+++ app/profile/ProfileForm.jsx
@@ -18,31 +18,36 @@ export default function ProfileForm({ profile }) {
         <form action={serverAction} className="flex flex-col gap-y-5">
 
             <label>
-                <img src={`/avatars/${profile.avatar}`} alt="" />
+                {
+                    profile?.avatar ?
+                        (<img src={`/avatars/${profile?.avatar}`} alt="" />) :
+                        (<img src="/avatars/defaultAvatar.png" alt="" />)
+                }
+
                 <input hidden type="file" name="avatar" />
             </label>
 
-            <Input defaultValue={new Date(profile.dateOfBirth).toISOString().split('T')[0]} name="dateOfBirth" type="date" />
+            <Input defaultValue={profile?.dateOfBirth ? new Date(profile.dateOfBirth).toISOString().split('T')[0] : ''} name="dateOfBirth" type="date" />
 
             <div>
                 <p className="text-xl">Ваш пол:</p>
                 <div className="flex gap-x-5 mt-3">
                     <label>
                         <p>Мужской</p>
-                        <Input defaultChecked={profile.sex === 'male'} className="w-5" value="male" name="sex" type="radio" />
+                        <Input defaultChecked={profile?.sex === 'male'} className="w-5" value="male" name="sex" type="radio" />
                     </label>
                     <label>
                         <p>Женский</p>
-                        <Input defaultChecked={profile.sex === 'female'} className="w-5" value="female" name="sex" type="radio" />
+                        <Input defaultChecked={profile?.sex === 'female'} className="w-5" value="female" name="sex" type="radio" />
                     </label>
                 </div>
             </div>
 
-            <Textarea defaultValue={profile.aboutMe} name="aboutMe" placeholder="Введите информацию о себе"></Textarea>
+            <Textarea defaultValue={profile?.aboutMe} name="aboutMe" placeholder="Введите информацию о себе"></Textarea>
 
-            <CountryPicker defaultCountry={profile.country} />
+            <CountryPicker defaultCountry={profile?.country} />
 
-            <CityPicker defaultCity={profile.city} />
+            <CityPicker defaultCity={profile?.city} />
 
             <Button disabled={pending} type="submit">Сохранить</Button>
 
